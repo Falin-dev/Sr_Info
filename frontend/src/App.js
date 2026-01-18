e5ed1f6ae11dee823c4d250698494889e01d4beb
Falin-dev <mounsandofalin.ai23@stellamaryscoe.edu.in>
2026-01-18 16:04:34 +0530

    chore: update API_URL to production backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://sr-info.onrender.com/';
 
 function App() {
   const [file, setFile] = useState(null);
